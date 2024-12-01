cc2b6ecfbe0661f2717bae287f775a9a8851cd76
Max Verbeek <m4xv3rb33k@gmail.com>
2024-12-02 00:29:17 +0100

    feat(day 01): 124 tokens

--- 01/main.go
+++ 01/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	// super nasty, but not having to type fmt. or sort. etc saves 2 tokens each time i use these functions
 	. "fmt"
+	. "math"
 	. "sort"
 )
 
@@ -11,12 +12,11 @@ func main() {
 	appearances := map[int]int{}
 
 	var left, right, sum, sim int
+	var err error
 
-	for {
+	for err == nil {
 		// this if-statement is necessary because scanf has 2 return values, and you cannot use 2 return values in a loop
-		if _, err := Scanf("%d   %d\n", &left, &right); err != nil {
-			break
-		}
+		_, err = Scanf("%d   %d\n", &left, &right)
 		first, second = append(first, left), append(second, right)
 		appearances[right]++
 	}
@@ -26,12 +26,9 @@ func main() {
 	Ints(second)
 
 	for i, left := range first {
-		right := second[i]
-
-		sum += max(left-right, right-left)
+		sum += int(Abs(float64(left - second[i])))
 		sim += left * appearances[left]
 	}
 
-	println(sum)
-	println(sim)
+	println(sum, sim)
 }
