18b8c429d6b616b603bbdc31cdaa8317fc35e321
Max Verbeek <m4xv3rb33k@gmail.com>
2024-12-11 16:03:54 +0100

    feat(day 11): add day 11

--- 11/main.go
+++ 11/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -20,11 +21,52 @@ func main() {
 		stones[i] = stone
 	}
 
-	for blink := 0; blink < 25; blink++ {
-		stones = NewStones(stones)
+	hist := Histogram(stones)
+	for blink := 0; blink < 75; blink++ {
+		newstones := map[int64]int64{}
+
+		for stone, count := range hist {
+			for _, newstone := range EvolveStone(stone) {
+				newstones[newstone] += count
+			}
+		}
+
+		hist = newstones
+
+		if blink == 24 {
+			println(CountHist(hist))
+		}
 	}
 
-	println(len(stones))
+	println(CountHist(hist))
+}
+
+func Largest(stones []int64) int64 {
+	return slices.Max(stones)
+}
+
+func CountHist(hist map[int64]int64) int64 {
+	total := int64(0)
+
+	for _, count := range hist {
+		total += count
+	}
+
+	return total
+}
+func Histogram(stones []int64) map[int64]int64 {
+	histogram := map[int64]int64{}
+	for _, stone := range stones {
+		histogram[stone]++
+	}
+
+	return histogram
+}
+
+func CountUnique(stones []int64) int {
+	sorted := slices.Clone(stones)
+	slices.Sort(sorted)
+	return len(slices.Compact(sorted))
 }
 
 func Log10(num int64) int64 {
@@ -46,6 +88,22 @@ func Pow10(pow int64) int64 {
 	}
 
 	return num
+}
+
+func EvolveStone(stone int64) []int64 {
+	newstones := []int64{}
+
+	digitcount := Log10(stone)
+	if stone == 0 {
+		newstones = append(newstones, 1)
+	} else if digitcount%2 == 0 {
+		halfpow := Pow10(digitcount / 2)
+		newstones = append(newstones, stone/halfpow, stone%halfpow)
+	} else {
+		newstones = append(newstones, stone*2024)
+	}
+
+	return newstones
 }
 
 func NewStones(oldstones []int64) []int64 {
