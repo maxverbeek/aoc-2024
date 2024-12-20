9a4ed208a17a0513702e9ca1475272cdecef82dd
Max Verbeek <m4xv3rb33k@gmail.com>
2024-12-20 22:54:23 +0100

    feat(day 19): add day 19

--- 19/main.go
+++ 19/main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"os"
-	"slices"
 	"strings"
 )
 
@@ -12,22 +10,48 @@ func main() {
 	input, _ := io.ReadAll(os.Stdin)
 	split := strings.Split(string(input), "\n\n")
 	towels := strings.Split(split[0], ", ")
-	combinations := strings.Split(split[1], "\n")
+	combinations := strings.Split(strings.TrimSpace(split[1]), "\n")
 
-	combinations = combinations
-
-	slices.Sort(towels)
-
-	fmt.Printf("%d %+v\n", len(towels), towels)
-}
-
-func Possible(towels []string, combination string) {
-	used := map[string]bool{}
-	longest := 0
+	usedtowels := map[string]bool{}
+	previouscombinations := map[string]int{}
 
 	for _, towel := range towels {
-		used[towel] = false
-		longest = max(longest, len(towel))
+		usedtowels[towel] = false
 	}
 
+	part1 := 0
+	part2 := 0
+
+	for _, combination := range combinations {
+		if p := Possibilities(usedtowels, combination, &previouscombinations); p > 0 {
+			part1++
+			part2 += p
+		}
+	}
+
+	println(part1, part2)
+}
+
+func Possibilities(towels map[string]bool, combination string, combinations *map[string]int) int {
+	if len(combination) == 0 {
+		return 1
+	}
+
+	if c, ok := (*combinations)[combination]; ok {
+		return c
+	}
+
+	p := 0
+
+	defer func() {
+		(*combinations)[combination] = p
+	}()
+
+	for towel := range towels {
+		if strings.HasPrefix(combination, towel) {
+			p += Possibilities(towels, combination[len(towel):], combinations)
+		}
+	}
+
+	return p
 }
