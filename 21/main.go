936b85e3dc0974b8271a5d572d3e5bfb585b0825
Max Verbeek <m4xv3rb33k@gmail.com>
2024-12-22 14:13:56 +0100

    refactor: remove bad solution

--- 21/main.go
+++ 21/main.go
@@ -36,79 +36,6 @@ var (
 	}
 )
 
-type CacheKey struct {
-	from rune
-	rest string
-}
-
-func AddLtGt(target, coordinate int, lt, gt rune) []rune {
-	result := []rune{}
-
-	for coordinate < target {
-		result = append(result, lt)
-		coordinate++
-	}
-
-	for coordinate > target {
-		result = append(result, gt)
-		coordinate--
-	}
-
-	return result
-}
-
-func TypeCombinations(keypad map[rune]Tuple, cache map[CacheKey][][]rune, from rune, combination []rune) [][]rune {
-	cachekey := CacheKey{from: from, rest: string(combination)}
-	if results, ok := cache[cachekey]; ok {
-		return results
-	}
-	prefixes := [][]rune{}
-
-	to := combination[0]
-
-	// horizontal means we're moving horizontally first, so x will change
-	// and y will stay at the from. the corner that this makes cannot end
-	// up on the empty square
-	if keypad[' '] != (Tuple{x: keypad[to].x, y: keypad[from].y}) {
-		prefix := []rune{}
-		prefix = append(prefix, AddLtGt(keypad[to].x, keypad[from].x, '>', '<')...)
-		prefix = append(prefix, AddLtGt(keypad[to].y, keypad[from].y, 'v', '^')...)
-		prefix = append(prefix, 'A')
-
-		prefixes = append(prefixes, prefix)
-	}
-
-	// !horizontal = vertical means y changes first, and x remains at the from position.
-	if keypad[' '] != (Tuple{x: keypad[from].x, y: keypad[to].y}) {
-		prefix := []rune{}
-		prefix = append(prefix, AddLtGt(keypad[to].x, keypad[from].x, '>', '<')...)
-		prefix = append(prefix, AddLtGt(keypad[to].y, keypad[from].y, 'v', '^')...)
-		prefix = append(prefix, 'A')
-
-		prefixes = append(prefixes, prefix)
-	}
-
-	if len(combination) > 1 {
-		suffixes := TypeCombinations(keypad, cache, to, combination[1:])
-
-		results := [][]rune{}
-
-		for _, prefix := range prefixes {
-			for _, suffix := range suffixes {
-				results = append(results, []rune(string(prefix)+string(suffix)))
-			}
-		}
-
-		cache[cachekey] = results
-
-		fmt.Printf("made %d results\n", len(results))
-
-		return results
-	}
-
-	return prefixes
-}
-
 func CombineGreedy(keypad map[rune]Tuple, combination []rune) []rune {
 	result := []rune{}
 	from := 'A'
@@ -182,52 +109,6 @@ func CombineGreedy(keypad map[rune]Tuple, combination []rune) []rune {
 	return result
 }
 
-func Allpads(pads []map[rune]Tuple, combination []rune) [][]rune {
-	if len(pads) == 0 {
-		return [][]rune{combination}
-	}
-
-	fmt.Printf("%d pads to go..\n", len(pads))
-
-	options := TypeCombinations(pads[0], make(map[CacheKey][][]rune), 'A', combination)
-
-	next := [][]rune{}
-
-	minlength, mincount := len(options[0]), 0
-
-	for i := range options {
-		if len(options[i]) < minlength {
-			minlength = len(options[i])
-			mincount = 1
-		} else if len(options[i]) == minlength {
-			mincount++
-		}
-	}
-
-	fmt.Printf("considering %d options on %d pads\n", mincount, len(pads))
-
-	for _, option := range options {
-		if len(option) == minlength {
-			next = append(next, Allpads(pads[1:], option)...)
-		}
-	}
-
-	return next
-}
-
-func RobotIndirection(combination []rune) []rune {
-	combinations := Allpads([]map[rune]Tuple{numpad, dirpad, dirpad}, combination)
-	minidx, minlength := len(combinations[0]), 0
-
-	for i := range combinations {
-		if len(combinations[i]) < minlength {
-			minidx, minlength = i, len(combinations[i])
-		}
-	}
-
-	return combinations[minidx]
-}
-
 func EncodePadsGreedy(pads []map[rune]Tuple, combination []rune) []rune {
 	for i, pad := range pads {
 		fmt.Printf("encoding pad %d with length %d\n", i, len(combination))
@@ -242,12 +123,10 @@ func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
 	part1maps := []map[rune]Tuple{numpad, dirpad, dirpad}
-	part2maps := make([]map[rune]Tuple, 27)
-
-	part2maps[0] = numpad
+	part2maps := []map[rune]Tuple{numpad}
 
 	for i := 0; i < 26; i++ {
-		part2maps[i+1] = dirpad
+		part2maps = append(part2maps, dirpad)
 	}
 
 	for scanner.Scan() {
