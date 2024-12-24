155bad2949e0a35608828a9b715d13fe38795ea4
Max Verbeek <m4xv3rb33k@gmail.com>
2024-12-24 15:42:20 +0100

    chore(day 23): too slow

--- 23/main.go
+++ 23/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"maps"
 	"os"
+	"reflect"
 	"slices"
 	"strings"
 )
@@ -84,19 +85,81 @@ func main() {
 
 	println(len(triples))
 
-	mostconnections := 0
-	mostconnected := []string{}
-
-	for comp, edges := range graph {
-		if len(edges) > mostconnections {
-			mostconnections = len(edges)
-			mostconnected = []string{comp}
-		} else if len(edges) == mostconnections {
-			mostconnected = append(mostconnected, comp)
-		}
+	// start out with fully connected singleton graphs
+	fcsubgraphs := []map[string]struct{}{}
+	for node := range graph {
+		fcsubgraphs = append(fcsubgraphs, map[string]struct{}{node: struct{}{}})
 	}
 
-	slices.Sort(mostconnected)
+	// iterate over all edges of the graph, and check for each edge if the
+	// connected node not yet part of the graph is connected to every node in
+	// the graph
 
-	println(strings.Join(mostconnected, ","))
+	degree := 1
+
+	for {
+		// increase each subgraph by 1 degree
+		nextdegreesubgraphs := []map[string]struct{}{}
+		for _, sg := range fcsubgraphs {
+			// keep track of neighbours in a set so we don't consider duplicates
+			neighbours := map[string]struct{}{}
+
+			for node := range sg {
+				for neighbour := range graph[node] {
+					if len(graph[neighbour]) <= degree {
+						// this neighbour does not have enough edges to be a reasonable candidate
+						continue
+					}
+					neighbours[neighbour] = struct{}{}
+				}
+			}
+
+			// for each neighbour, check if they are connected to every node in
+			// the graph. if so we have a new subgraph of SG + [neighbour]
+			for neighbour := range neighbours {
+				connected := true
+				for node := range sg {
+					if !graph[node][neighbour] {
+						connected = false
+						break
+					}
+				}
+
+				if connected {
+					sg2 := maps.Clone(sg)
+					sg2[neighbour] = struct{}{}
+					nextdegreesubgraphs = append(nextdegreesubgraphs, sg2)
+				}
+			}
+		}
+
+		// prune next degree subgraphs, because there should be duplicates when
+		// 2 different graphs merge
+		for i := len(nextdegreesubgraphs) - 1; i >= 0; i-- {
+			for j := range nextdegreesubgraphs[:len(nextdegreesubgraphs)-i-1] {
+				if reflect.DeepEqual(nextdegreesubgraphs[i], nextdegreesubgraphs[j]) {
+					// swap with last element of the list
+					nextdegreesubgraphs = RemoveFromSlice(nextdegreesubgraphs, i)
+					break
+				}
+			}
+		}
+
+		if len(nextdegreesubgraphs) == 0 {
+			break
+		}
+
+		fcsubgraphs = nextdegreesubgraphs
+		degree++
+
+		println(degree)
+	}
+
+	println(strings.Join(slices.Sorted(maps.Keys(fcsubgraphs[0])), ","))
+}
+
+func RemoveFromSlice[T any](slice []T, idx int) []T {
+	last := len(slice) - 1
+	slice[idx] = slice[last]
+	return slice[:last]
 }
